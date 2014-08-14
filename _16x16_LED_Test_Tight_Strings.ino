64bb6d873214befeb7db98df12023722c11f8dfa
Matt Levine <mtlevine0@gmail.com>
2014-08-14 01:51:34 -0400

    Added ability for 32 px height. untested

--- _16x16_LED_Test_Tight_Strings.ino
+++ _16x16_LED_Test_Tight_Strings.ino
@@ -14,6 +14,8 @@ int latchPin = 10;
 int clockPin = 8;
 int dataPinA = 9;
 int dataPinB = 12;
+int dataPinC = 7;
+int dataPinD = 6;
 
 uint8_t buffer[128];
 String message = "#HIGHFEST brought to you by: 36 N. High St. #THEZOO               ";
@@ -24,46 +26,41 @@ void setup(void){
   pinMode(outputEn, OUTPUT);
   pinMode(dataPinA, OUTPUT);
   pinMode(dataPinB, OUTPUT);
+  pinMode(dataPinC, OUTPUT);
+  pinMode(dataPinD, OUTPUT);
   analogWrite(outputEn, 250);
-  Serial.begin(9600);
 }
 
 void loop(void){
-   //for(uint8_t letter = 0; letter < 95; letter++){// Scroll through entire font  
-  for(uint8_t letter = 0; letter < message.length(); letter++){// Scroll through entire font  
-    //for(uint8_t i = 0; i < fontInfo[letter][0]*2 + 1; i+=2){
-    for(uint8_t i = 0; i < fontInfo[message[letter]-32][0]*2 + 1; i+=2){
-      //if(fontInfo[letter][0]*2 == i){// Place one pixel spaces between each character
-      if(fontInfo[message[letter]-32][0]*2 == i){// Place one pixel spaces between each character
-        buffer[127]=0x00;
-        buffer[126]=0x00;
-        for(uint8_t j = 0; j < 126; j+=2){
-          buffer[j] = buffer[j+2];
-          buffer[j+1] = buffer[j+3];
-        }
-      }else{    
-      //buffer[127] = pgm_read_byte_near(font + fontInfo[letter][1] + i + 1);
-      //buffer[126] = pgm_read_byte_near(font + fontInfo[letter][1] + i);
-      buffer[127] = pgm_read_byte_near(font + fontInfo[message[letter]-32][1] + i + 1);
-      buffer[126] = pgm_read_byte_near(font + fontInfo[message[letter]-32][1] + i);
-      
-      for(uint8_t j = 0; j < 126; j+=2){// Scroll buffer one pixel at a time
-        buffer[j] = buffer[j+2];
-        buffer[j+1] = buffer[j+3];
-      }
-      }
-      writeDisplay();// Write buffer to display
-      delay(10);
-    }
-    /*
-    buffer[127]=0x00;
-    buffer[126]=0x00;
-    for(uint8_t j = 0; j < 126; j+=2){// Scroll buffer by one
-        buffer[j] = buffer[j+2];
-        buffer[j+1] = buffer[j+3];
-    }
-    */
-  }
+	for(uint8_t letter = 0; letter < 95; letter++){// Scroll through entire font  
+		for(uint8_t i = 0; i < fontInfo[letter][0]*2 + 1; i+=2){
+      		if(fontInfo[letter][0]*2 == i){// Place one pixel spaces between each character
+				buffer[127]=0x00;
+				buffer[126]=0x00;
+				buffer[125]=0x00;
+				buffer[124]=0x00;
+				for(uint8_t j = 0; j < 124; j+=4){
+					buffer[j] = buffer[j+4];
+				  	buffer[j+1] = buffer[j+5];
+				  	buffer[j+2] = buffer[j+6];
+				  	buffer[j+3] = buffer[j+7];
+				}
+			}else{    
+				buffer[127] = pgm_read_byte_near(font + fontInfo[letter][1] + i + 3);
+				buffer[126] = pgm_read_byte_near(font + fontInfo[letter][1] + i + 2);
+				buffer[125] = pgm_read_byte_near(font + fontInfo[letter][1] + i + 1);
+				buffer[124] = pgm_read_byte_near(font + fontInfo[letter][1] + i);
+				for(uint8_t j = 0; j < 124; j+=4){// Scroll buffer one pixel at a time
+					buffer[j] = buffer[j+4];
+					buffer[j+1] = buffer[j+5];
+					buffer[j+2] = buffer[j+6];
+					buffer[j+3] = buffer[j+7];
+				}
+			}
+			writeDisplay();// Write buffer to display
+			delay(10);
+		}
+	}
 } 
 
 //
@@ -76,21 +73,27 @@ void halt(void){
 
 void writeDisplay(void){
   digitalWrite(latchPin, LOW); 
-  for(uint8_t i = 0; i < 128; i+=2){//32
-    shiftOut(dataPinA, dataPinB, clockPin, MSBFIRST, buffer[i], buffer[i+1]);
+  for(uint8_t i = 0; i < 128; i+=4){//32
+    shiftOut(dataPinA, dataPinB, dataPinC, dataPinD, clockPin, MSBFIRST, buffer[i], buffer[i+1], buffer[i+2], buffer[i+3]);
   }
   digitalWrite(latchPin, HIGH);
 }
 
-void shiftOut(uint8_t dataPinA, uint8_t dataPinB, uint8_t clockPin, uint8_t bitOrder, uint8_t valA, uint8_t valB){
+void shiftOut(uint8_t dataPinA, uint8_t dataPinB, uint8_t dataPinC, uint8_t dataPinD, uint8_t clockPin, uint8_t bitOrder, uint8_t valA, uint8_t valB, uint8_t valC, uint8_t valD){
   uint8_t i;
   for (i = 0; i < 8; i++)  {
     if(bitOrder == LSBFIRST){
       digitalWrite(dataPinA, !!(valA & (1 << i)));
       digitalWrite(dataPinB, !!(valB & (1 << i)));
+      digitalWrite(dataPinC, !!(valC & (1 << i)));
+      digitalWrite(dataPinD, !!(valD & (1 << i)));
+
     }else{      
       digitalWrite(dataPinA, !!(valA & (1 << (7 - i))));
       digitalWrite(dataPinB, !!(valB & (1 << (7 - i))));
+      digitalWrite(dataPinC, !!(valC & (1 << (7 - i))));
+      digitalWrite(dataPinD, !!(valD & (1 << (7 - i))));
+
     }      
     digitalWrite(clockPin, HIGH);
     digitalWrite(clockPin, LOW);            
