76ca376616bdd86ff22ea7601deec541ae88baf5
Matt Levine <mtlevine0@gmail.com>
2014-08-14 01:58:04 -0400

    Cleaned up code

--- _16x16_LED_Test_Tight_Strings.ino
+++ _16x16_LED_Test_Tight_Strings.ino
@@ -29,11 +29,9 @@ void setup(void){
 }
 
 void loop(void){
-   //for(uint8_t letter = 0; letter < 95; letter++){// Scroll through entire font  
-  for(uint8_t letter = 0; letter < message.length(); letter++){// Scroll through entire font  
-    //for(uint8_t i = 0; i < fontInfo[letter][0]*2 + 1; i+=2){
+  int length = message.length();
+  for(uint8_t letter = 0; letter < length; letter++){// Scroll through entire font  
     for(uint8_t i = 0; i < fontInfo[message[letter]-32][0]*2 + 1; i+=2){
-      //if(fontInfo[letter][0]*2 == i){// Place one pixel spaces between each character
       if(fontInfo[message[letter]-32][0]*2 == i){// Place one pixel spaces between each character
         buffer[127]=0x00;
         buffer[126]=0x00;
@@ -42,27 +40,16 @@ void loop(void){
           buffer[j+1] = buffer[j+3];
         }
       }else{    
-      //buffer[127] = pgm_read_byte_near(font + fontInfo[letter][1] + i + 1);
-      //buffer[126] = pgm_read_byte_near(font + fontInfo[letter][1] + i);
-      buffer[127] = pgm_read_byte_near(font + fontInfo[message[letter]-32][1] + i + 1);
-      buffer[126] = pgm_read_byte_near(font + fontInfo[message[letter]-32][1] + i);
-      
-      for(uint8_t j = 0; j < 126; j+=2){// Scroll buffer one pixel at a time
-        buffer[j] = buffer[j+2];
-        buffer[j+1] = buffer[j+3];
-      }
+        buffer[127] = pgm_read_byte_near(font + fontInfo[message[letter]-32][1] + i + 1);
+        buffer[126] = pgm_read_byte_near(font + fontInfo[message[letter]-32][1] + i);
+        for(uint8_t j = 0; j < 126; j+=2){// Scroll buffer one pixel at a time
+          buffer[j] = buffer[j+2];
+          buffer[j+1] = buffer[j+3];
+        }
       }
       writeDisplay();// Write buffer to display
-      delay(10);
+      //delay(10);
     }
-    /*
-    buffer[127]=0x00;
-    buffer[126]=0x00;
-    for(uint8_t j = 0; j < 126; j+=2){// Scroll buffer by one
-        buffer[j] = buffer[j+2];
-        buffer[j+1] = buffer[j+3];
-    }
-    */
   }
 } 
 
@@ -70,13 +57,18 @@ void loop(void){
 // User functions
 //
 
+void allOn(void){
+  
+}
+
 void halt(void){
    while(true){} 
 }
 
 void writeDisplay(void){
-  digitalWrite(latchPin, LOW); 
-  for(uint8_t i = 0; i < 128; i+=2){//32
+  //digitalWrite(latchPin, LOW);
+  PORTB = PORTB | (1<<2);
+  for(uint8_t i = 0; i < 128; i+=2){
     shiftOut(dataPinA, dataPinB, clockPin, MSBFIRST, buffer[i], buffer[i+1]);
   }
   digitalWrite(latchPin, HIGH);
